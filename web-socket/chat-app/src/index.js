2de259a5a6f0069c5861444b591dccf225493351
srikanth019 <srikanth.golla@brainvire.com>
2024-05-22 10:31:03 +0530

    Completed sending msg to individual rooms

---
+++ web-socket/chat-app/src/index.js
@@ -0,0 +1,103 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
+
+const {
+  addUser,
+  getUser,
+  getUsersInRoom,
+  removeUser,
+  getAllUsers,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  //   socket.emit("message", generateMessage("Welcome!"));
+  //   socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage("Admin", `${user.username} has joined!`)
+      );
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
+
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+    callback();
+  });
+
+  socket.on("sendLocation", (coords, callback) => {
+    const user = getUser(socket.id);
+    io.to(user?.room).emit(
+      "locationMessage",
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
+    );
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const removedUser = removeUser(socket.id);
+    if (removedUser) {
+      io.to(removedUser.room).emit(
+        "message",
+        generateMessage(`${removedUser.username} has left!`)
+      );
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
+
+//socket.emit => Sends an event specific client
+
+//io.emit => Sends an event every connected clients
+
+//socket.broadcast.emit => Sends an event to all clients connected except the current one
+
+// For Rooms
+
+// io.to(ROOM_NAME).emit => Sends an event to every connected user in the room
+
+//socket.broadcast.to(ROOM_NAME).emit => Sends an event to all clients connected except the current one
